6b70e89cf30fabf2bcc39ea081a25ab2b6890ac8
prabalpk <prabalpk12@gmail.com>
2025-12-04 18:29:48 +0530

    final_last_1

---
+++ math_module_for _calculation.py
@@ -0,0 +1,22 @@
+# import math
+# def calculate_square_root(number):
+#     return math.sqrt(number)
+# def logarithm(number):
+#     return math.log(number)
+# def sine(number):
+#     return math.sin(number)
+
+
+import math
+
+
+num = float(input("Enter a number: "))
+
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print(f"Square Root: {sqrt_val}")
+print(f"Logarithm: {log_val}")
+print(f"Sine: {sine_val}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import math
+# def calculate_square_root(number):
+#     return math.sqrt(number)
+# def logarithm(number):
+#     return math.log(number)
+# def sine(number):
+#     return math.sin(number)
+
